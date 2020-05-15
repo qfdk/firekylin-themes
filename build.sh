28372a552f2ba32de97704430d05454b146f2ad8
qfdk <qfdk2010@gmail.com>
2020-05-15 07:32:50 +0200

    first commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# 删除临时文件
+rm -rf output;
+rm -rf firekylin;
+# 复制新主题
+cp -r ../www/theme/firekylin .;
+# 产生新主题
+node stc.config.js;
+
+sed -i 's/firekylin/firekylin.build/' output/firekylin/package.json;
+rm -rf mkdir ../www/theme/firekylin.build;
+mkdir ../www/theme/firekylin.build;
+cp -r output/firekylin/* ../www/theme/firekylin.build;
---
+++ stc.config.js
@@ -0,0 +1,22 @@
+const stc = require("stc");
+const htmlCompress = require("stc-html-compress");
+const cssCombine = require("stc-css-combine");
+const cssCompress = require("stc-css-combine");
+const uglify = require("stc-uglify");
+stc.config({
+  include: ["firekylin"],
+  outputPath: 'firekylin.build',
+  product: "speech",
+  tpl: {
+    engine: "nunjucks",
+    ld: ["{{", "{%", "{="],
+    rd: ["}}", "%}", "=}"],
+  },
+});
+stc.workflow({
+  uglify: { plugin: uglify, include: /\.js$/, options: {} },
+  cssCombine: { plugin: cssCombine, include: /\.css$/, options: {} },
+  cssCompress: { plugin: cssCompress, include: /\.css$/, options: {} },
+  htmlCompress: { plugin: htmlCompress, include: /\.html$/, options: {} },
+});
+stc.start(); // 启动工作流程
